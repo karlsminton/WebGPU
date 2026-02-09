e08b32e263714fd16e0eb1c4189d3fec0988c51c
Karl Minton <kminton@frontier.co.uk>
2026-02-09 20:31:30 +0000

    working basic server with https

---
+++ README.md
@@ -0,0 +1,9 @@
+# WebGPU
+
+### Installation
+- `openssl req -x509 -newkey rsa:2048 -keyout keytmp.pem -out cert.pem -days 365`
+- `openssl rsa -in keytmp.pem -out key.pem`
+- `npm run build-client`
+- `npm run build-server`
+- `npm run start`
+
--- server.ts
+++ server.ts
@@ -1,13 +1,21 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const key: string = fs.readFileSync('./key.pem');
+const cert: string = fs.readFileSync('./cert.pem');
 
 const app = express();
 app.use(express.static(__dirname + '/static'));
 const port = 6543;
 
+const server = https.createServer({key: key, cert: cert}, app);
+
 app.get('/', (req: any, res: any) => {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(path.resolve('./dist/index.html'));
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
